ff9825d188069908da0defcc7f7c22cd8a9cb37a
Chalomba <chalomba@macbook-air-mac-2.local>
2016-05-24 19:12:16 +0300

    my Home work1

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Chalomba on 20.05.16.
+//  Copyright © 2016 Chalomba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
